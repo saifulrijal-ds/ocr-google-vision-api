7cfb9b51743006661046771cb4e9e11f0bf6a543
Saiful Rijal <46024788+saifulrijal-ds@users.noreply.github.com>
2022-09-08 16:39:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
-from __future__ import print_function
 from google.cloud import vision
 import io
 
-image_uri = st.text_input("Input license plate image uri", "")
-st.write(image_uri)
+image_uri = st.text_input("Enter the URI of the license plate image", "")
+st.write(f"Image URI: {image_uri}")
